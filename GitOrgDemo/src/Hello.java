abbaa743ca412967e993c3cad316195681b25597
SangeethaPriyaS <sgeethasaravana29@gmail.com>
2020-06-03 11:42:29 +0530

    Create Hello.java
    New file added

---
+++ GitOrgDemo/src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GIT");
+		
+		System.out.println("Added by Sarathy");
+		System.out.println("This is a Test by Sarathy NEW");
+		System.out.println("Added by Sangeetha 4");
+	}
+
+}
